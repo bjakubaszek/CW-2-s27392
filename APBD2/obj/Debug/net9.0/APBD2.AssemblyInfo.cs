8a184d8fb80a16ac5972a0361dff319a99abe2e3
bjakubaszek <s27392@pjwstk.edu.pl>
2025-03-23 19:54:01 +0100

    test

--- APBD2/obj/Debug/net9.0/APBD2.AssemblyInfo.cs
+++ APBD2/obj/Debug/net9.0/APBD2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d348a75902e8b647b1e2bb41b7932fccc2a3c001")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
